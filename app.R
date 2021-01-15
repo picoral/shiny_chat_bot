6586793582985a20ed340144c3998af0558d690d
picoral <adrianaps@email.arizona.edu>
2021-01-15 14:28:48 -0700

    added my email address as a comment

--- app.R
+++ app.R
@@ -2,6 +2,8 @@
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
 #
+# This application was created by Adriana Picoral (adrianaps@arizona.edu)
+#
 # Find out more about building applications with Shiny here:
 #
 #    http://shiny.rstudio.com/
